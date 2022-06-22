6707918552bacd9f2e37664258e1c6413b40a188
ebikiel <ebikiel@pickit.net>
2022-06-22 02:53:34 -0300

    Cerrando Node

--- src/opinions/opinions.controller.ts
+++ src/opinions/opinions.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Post, Body, Param, UseGuards, Query, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards, Query, Request, UseInterceptors } from '@nestjs/common';
 import { OpinionsService } from './opinions.service';
 import { CreateOpinionDto } from './dto/create-opinion.dto';
 import { Roles } from 'src/auth/role.decorator';
 import { Role } from 'src/auth/role.enum';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/roles.guard';
+import { TransformInterceptor } from 'src/transform.interceptor';
 
 @Controller('opinions')
 export class OpinionsController {
   constructor(private readonly opinionsService: OpinionsService) {}
 
+  @UseInterceptors(TransformInterceptor)
   @Roles(Role.Tourist)
   @UseGuards(JwtAuthGuard,RolesGuard)
   @Post()
@@ -17,6 +19,7 @@ export class OpinionsController {
     return this.opinionsService.create(req.user.id, createOpinionDto);
   }
 
+  @UseInterceptors(TransformInterceptor)
   @Roles(Role.Provider)
   @UseGuards(JwtAuthGuard,RolesGuard)
   @Get("event/:eventId")
